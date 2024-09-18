53f21c9a916f886e431c42eaea226b0fb8212c24
kamenifg01 <kamenifg@gmail.com>
2024-09-18 12:00:44 +0200

    Suppression de tous les fichiers pour réinitialisation

--- backend/node/config/firebase.js
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../../lyonmall-firebase-adminsdk-a9ozx-64c3b45018.json'); // Téléchargez ce fichier depuis Firebase Console
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
