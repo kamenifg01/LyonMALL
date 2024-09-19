3d3e264c75b985f55c8f82672ab8561d39a8ffda
kamenifg01 <kamenifg@gmail.com>
2024-09-19 21:42:23 +0200

    config json

--- backend/node/config/firebase.js
+++ backend/node/config/firebase.js
@@ -1,8 +1,14 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../../lyonmall-firebase-adminsdk-a9ozx-64c3b45018.json'); // Téléchargez ce fichier depuis Firebase Console
+// backend/node/config/firebase.js
+
+import admin from 'firebase-admin';
+import { readFileSync } from 'fs';  // Utilisation de fs pour lire le fichier JSON
+import { resolve } from 'path';  // Pour obtenir le chemin absolu du fichier
+
+const serviceAccountPath = resolve('../../backend/node/config/lyonmall-firebase-adminsdk-a9ozx-64c3b45018.json');
+const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf8'));  // Lire le fichier JSON
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-module.exports = admin;
+export default admin;
