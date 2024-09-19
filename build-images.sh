147b7c1d7cf4433218096f2131cfca1a097c459a
kamenifg01 <kamenifg@gmail.com>
2024-09-19 22:54:01 +0200

    Reset branch

--- build-images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Construire l'image Docker pour Node.js
-cd frontend/node-app
-docker build -t node-app:latest .
-
-# Construire l'image Docker pour Django
-cd ../../backend/django-app
-docker build -t django-app:latest .
--- deploy-to-kubernetes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Appliquer les fichiers de déploiement Kubernetes
-kubectl apply -f kubernetes/node-deployment.yaml
-kubectl apply -f kubernetes/django-deployment.yaml
